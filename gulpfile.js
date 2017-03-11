85badcf1ec1a0eac13b2eafd79a652f5aa243a7e
ayame30 <alnaym30@gmail.com>
2017-03-12 04:33:00 +0800

    Revamp

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 
 gulp.task('watch', function () {
-	gulp.watch('./scss/**/*.scss', ['sass']);
+	gulp.watch('./resource/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('sass', function(){
-	gulp.src('./resource/scss/**/*.scss')
+	gulp.src('./resource/scss/style.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./public/css/'));
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,45 @@
 $(function(){
+    $(window).on('scroll init resize', function(){
+        var height = $(window).height()/2;
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > height*3) {
+            $('#intro')
+                .addClass('play-end')
+                .addClass('play-1')
+                .addClass('play-2');
+        } else if (scrollTop > height*2) {
+            $('#intro')
+                .removeClass('play-end')
+                .addClass('play-1')
+                .addClass('play-2');
+        } else if (scrollTop > height) {
+            $('#intro')
+                .removeClass('play-end')
+                .addClass('play-1')
+                .removeClass('play-2');
+        } else {
+            $('#intro')
+                .removeClass('play-end')
+                .removeClass('play-1')
+                .removeClass('play-2');
+        }
+        if (scrollTop >= $('#intro-wrapper')[0].offsetTop + $('#intro-wrapper')[0].offsetHeight) {
+            $('.header-space').addClass('active');
+            var navHeight = 37;
 
+        } else {
+            $('.header-space').removeClass('active');
+        }
 
+        if (scrollTop + $(window).height() >= $('.section-a')[0].offsetTop + $('.section-a')[0].offsetHeight) {
+            $('.footer').addClass('active');
+        } else {
+            $('.footer').removeClass('active');
+        }
+    });
+    $('.slick').slick({
+        infinite: false,
+    });
 
 
 });
